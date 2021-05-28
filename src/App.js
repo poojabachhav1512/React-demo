2ee9e85f994d3e1ff86b07b728a2a71fd2fdc5e7
poojabachhav555 <pooja.bachhav@neosoftmail.com>
2021-05-28 15:21:02 +0530

    React Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import Carousel from "./components/Carousel"
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Carousel></Carousel>
+     
+    </div>
+  );
+}
+
+export default App;
